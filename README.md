bda7de03d760a1460e299da33154caea5c26d04a
Eric Fulmer <EFulmer@gmail.com>
2016-02-22 01:46:15 -0500

    massive reorg of source code, cleanup of levelUp fn

---
+++ README.md
@@ -0,0 +1,4 @@
+haskell-fe
+==========
+
+Haskell port of the GBA Fire Emblem combat system. Currently *very* early in development; initial work will be on implementing characters and combat. Map system to come much later.
---
+++ src/Types.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE TemplateHaskell #-}
+module Types where
+import Control.Lens
+
+data Character = Character 
+    { _name :: String
+    , _klass :: String
+    , _level :: Int
+    , _xp :: Int
+    , _curHP :: Int -- need better way to represent
+    , _stats :: Stats
+    , _growths :: Stats
+    , _items :: [Weapon] } deriving Show
+
+data Stats = Stats 
+    { _hp :: Int
+    , _pow :: Int
+    , _skl :: Int
+    , _spd :: Int
+    , _lck :: Int
+    , _def :: Int
+    , _res :: Int } deriving Show
+
+data Weapon = Weapon 
+    { _wpName :: String
+    , _kind :: WeaponType
+    , _uses :: Int
+    , _mt :: Int
+    , _hit :: Int
+    , _crit :: Int
+    , _rng :: (Int, Int) } deriving Show -- range = (min, max)
+
+data WeaponType = Physical PhysWeapon | Magical MagWeapon deriving (Show, Eq)
+
+data PhysWeapon = Sword | Lance | Axe | Bow deriving (Show, Eq)
+
+data MagWeapon = Light | Dark | Anima deriving (Show, Eq)
+
+makeLenses ''Character
+makeLenses ''Stats
+makeLenses ''Weapon
+
