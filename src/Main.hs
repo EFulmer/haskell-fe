460d7c926fa72ad3defe6d42d4a324a2168cf3a7
Eric Fulmer <EFulmer@gmail.com>
2016-04-01 20:25:33 -0400

    baby's first main menu

--- src/Main.hs
+++ src/Main.hs
@@ -4,7 +4,20 @@ import Examples
 import LevelUp
 import Misc
 import Types
-    
+
+mainMenu = do 
+  putStrLn "Welcome to Haskell FE 0.0.1!"
+  putStrLn "Please select an option:"
+  putStrLn "1. Create a new character."
+  putStrLn "2. Battle two existing characters."
+  putStrLn "3. View existing characters."
+  option <- getLine
+  case option of
+    "1" -> undefined
+    "2" -> undefined
+    "3" -> undefined
+    _   -> putStrLn ("Unrecognized option: " ++ option) >> mainMenu
+
 -- TBD!
 main :: IO ()
-main = undefined
+main = mainMenu
